0889e1b07f2c226ed4bf835626f8ce53719bfed8
Keelana Char <keelanachar@gmail.com>
2020-08-10 21:28:54 -0500

    Logical and operator

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -44,5 +44,15 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And aliens are cool!");
         }
+
+        int topScore = 80;
+        if (topScore <= 100) {
+            System.out.println("High score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than 2nd top score and less than 100");
+        }
     }
 }

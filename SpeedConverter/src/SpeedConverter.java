dd06a87560a144b5d2fd857bbb1a46d0f9df4e95
Keelana Char <keelanachar@gmail.com>
2020-09-06 18:33:48 -0500

    Add Main class

--- SpeedConverter/src/SpeedConverter.java
+++ SpeedConverter/src/SpeedConverter.java
@@ -1,25 +1,21 @@
 public class SpeedConverter {
 
-    public static void main(String[] args) {
-        toMilesPerHour(-10);
-        printConversion(1.5);
-    }
-
     public static long toMilesPerHour(double kilometersPerHour) {
-        long roundedMilesPerHour = Math.round(kilometersPerHour / 1.609);
+
         if (kilometersPerHour < 0) {
             return -1;
-        } else {
-            return roundedMilesPerHour;
         }
+
+        return Math.round(kilometersPerHour / 1.609);
     }
 
     public static void printConversion(double kilometersPerHour) {
+
         if (kilometersPerHour < 0) {
             System.out.println("Invalid Value");
-        } else {
-            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
         }
+
+        System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
     }
 
 }

0e6d3a5bbb465c1a205a3c27d6ccad828c3f7479
Keelana Char <keelanachar@gmail.com>
2020-09-07 15:02:49 -0500

    Improved BarkingDog solution

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -2,13 +2,10 @@ public class BarkingDog {
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
 
-        if (!barking || hourOfDay < 0 || hourOfDay > 23) {
-            return false;
-        } else if (hourOfDay < 8 || hourOfDay > 22) {
+        if (barking == true && (hourOfDay >= 0 && (hourOfDay < 8 || hourOfDay > 22) && hourOfDay < 24)) {
             return true;
-        } else {
-            return false;
         }
+        return false;
 
     }
 

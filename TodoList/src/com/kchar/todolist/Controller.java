20b7ffe1417bc6ec5b130ac37d0882888edcdba4
Keelana Char <keelanachar@gmail.com>
2020-12-11 20:05:37 -0600

    Format date/time

--- TodoList/src/com/kchar/todolist/Controller.java
+++ TodoList/src/com/kchar/todolist/Controller.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +50,8 @@ public class Controller {
                 if(newValue != null) {
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
                 }
 
             }

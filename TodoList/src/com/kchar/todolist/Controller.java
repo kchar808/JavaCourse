7eb6e4e850953689930774190c2c2c431b68b6b6
Keelana Char <keelanachar@gmail.com>
2020-12-19 15:29:52 -0600

    Its working again

--- TodoList/src/com/kchar/todolist/Controller.java
+++ TodoList/src/com/kchar/todolist/Controller.java
@@ -46,7 +46,7 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+        todoListView.setItems(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
@@ -74,11 +74,7 @@ public class Controller {
         if(result.isPresent() && result.get() == ButtonType.OK) {
             DialogController controller = fxmlloader.getController();
             TodoItem newItem = controller.processResults();
-            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
             todoListView.getSelectionModel().select(newItem);
-            System.out.println("OK pressed");
-        } else {
-            System.out.println("Cancel pressed");
         }
 
     }
